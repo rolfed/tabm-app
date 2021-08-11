81b42f4572db602c6f969092dd985c0c35e85312
Danniel Rolfe <talkto@dannielrolfe.com>
2021-08-10 22:25:27 -0700

    Fix

--- src/component/About.svelte
+++ src/component/About.svelte
@@ -7,12 +7,12 @@
 	<div class="max-w-full md:max-w-md">
 		<p class="font-serif text-gray-500">
 			Lorep ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum ut mi ut mollis. Sed
-			facilisis dolor nec nulla convallis mattis. Suspendisse lacinia mauris sapien, ut lacinia lacus
-			feugiat quis. Phasellus at dui eros. Sed ac magna massa. Orci varius natoque penatibus et magnis
-			dis parturient montes, nascetur ridiculus mus. Praesent euismod faucibus risus tempor eleifend.
-			Proin molestie, sem sit amet iaculis fermentum, orci nisi commodo dolor, eget tincidunt tellus
-			nulla id nisl. Proin id justo sed risus lobortis viverra ut porttitor libero. Pellentesque felis
-			augue, sollicitudin quis mauris at, consectetur ullamcorper lorem.
+			facilisis dolor nec nulla convallis mattis. Suspendisse lacinia mauris sapien, ut lacinia
+			lacus feugiat quis. Phasellus at dui eros. Sed ac magna massa. Orci varius natoque penatibus
+			et magnis dis parturient montes, nascetur ridiculus mus. Praesent euismod faucibus risus
+			tempor eleifend. Proin molestie, sem sit amet iaculis fermentum, orci nisi commodo dolor, eget
+			tincidunt tellus nulla id nisl. Proin id justo sed risus lobortis viverra ut porttitor libero.
+			Pellentesque felis augue, sollicitudin quis mauris at, consectetur ullamcorper lorem.
 		</p>
 	</div>
 </section>
--- src/component/Contact.svelte
+++ src/component/Contact.svelte
@@ -2,38 +2,34 @@
 	import SectionTitle from './SectionTitle.svelte';
 </script>
 
-<style>
-</style>
-
 <section class="h-auto md:h-screen">
 	<SectionTitle title="Contact" />
 	<form class="grid grid-cols-1 gap-4 bg-gray-50 rounded p-4 md:p-32">
-		<label class="block text-gray-500">
-			Subject
-		</label>
-		<input type="text"
-					 class="font-serif shadow appearance-none border rounded w-full py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-					 id="subject"
-					 placeholder="Tell me something"
-					/>
-
-		<label class="block text-gray-500">
-			Email
-		</label>
-		<input type="email"
-					 class="font-serif shadow appearance-none border rounded w-full py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
-					 id="email"
-					 placeholder="email"
+		<label class="block text-gray-500" for="subject"> Subject </label>
+		<input
+			type="text"
+			class="font-serif shadow appearance-none border rounded w-full py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+			id="subject"
+			placeholder="Tell me something"
 		/>
 
-		<label class="block text-gray-500">
-			Message
-		</label>
+		<label class="block text-gray-500" for="email"> Email </label>
+		<input
+			type="email"
+			class="font-serif shadow appearance-none border rounded w-full py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+			id="email"
+			placeholder="email"
+		/>
+
+		<label class="block text-gray-500" for="message"> Message </label>
 		<textarea
 			class="font-serif shadow appearance-none border rounded w-full py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
 			placeholder="What do you have to say?"
-		/>
+			id="message"></textarea>
 
 		<button class="bg-yellow-200 rounded text-gray-50">Submit</button>
 	</form>
 </section>
+
+<style>
+</style>
